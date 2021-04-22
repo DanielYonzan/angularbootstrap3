22051ccfccca7808ce8731315de2177ececf42a9
shivaneupane3614 <shivaneupane3614@gmail.com>
2021-04-22 16:08:37 +0545

    observables

--- src/app/recipes/recipe.service.ts
+++ src/app/recipes/recipe.service.ts
@@ -31,8 +31,13 @@ export class RecipeService {
   }
 
 
-  getRecipes(){
+  getRecipes() {
   return this.recipes.slice();
+}
+
+getRecipe(index: number)
+{
+  return this.recipes[index];
 }
 addIngredientsToShoppingList(ingredients: Ingredient[]){
   this.slService.addIngredients(ingredients);
