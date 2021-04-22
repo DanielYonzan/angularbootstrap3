57aa789863f3942472948e60479f1b722ab3d0c7
danielyonzan <adtamang95@gmail.com>
2021-04-23 01:24:31 +0545

    lttle

--- src/app/recipes/recipe-list/recipe-list.component.ts
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, Input, OnInit} from '@angular/core';
 import {Recipe} from '../recipe.model';
 import {RecipeService} from '../recipe.service';
 
@@ -8,7 +8,7 @@ import {RecipeService} from '../recipe.service';
   styleUrls: ['./recipe-list.component.css']
 })
 export class RecipeListComponent implements OnInit {
-recipes: Recipe[];
+@Input() recipes: Recipe[];
 
   constructor(private recipeService: RecipeService) { }
 
