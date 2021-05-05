45402a2102cdaa7f0f7d87203781b8f247c974f1
Daniel Yonzan <adtamang95@gmail.com>
2021-05-05 17:23:57 +0545

    http

--- src/app/recipes/recipe.service.ts
+++ src/app/recipes/recipe.service.ts
@@ -8,27 +8,32 @@ import {Subject} from 'rxjs';
 export class RecipeService {
     recipesChanged = new Subject<Recipe[]>();
 
-    private recipes: Recipe[] = [
-        new Recipe('A Test Recipe',
-            'This is simply a Test',
-            'https://d47gxy3fatgwk.cloudfront.net/wp-content/uploads/2018/02/fall-apart-roasted-pork-shoulder-319.jpg',
-            [
-                new Ingredient('Meat', 1),
-                new Ingredient('French Fries', 20)
-            ]),
-        new Recipe('A Test2 Recipe',
-            'This is simply a Test2',
-            'https://img.taste.com.au/DzZTwETU/taste/2019/06/roast-pork-with-crispy-crackling-and-maple-mustard-pears-p50-150224-3.jpg',
-            [
-                new Ingredient('Buns', 2),
-                new Ingredient('Meat', 1)
-            ])
+    // private recipes: Recipe[] = [
+    //     new Recipe('A Test Recipe',
+    //         'This is simply a Test',
+    //         'https://d47gxy3fatgwk.cloudfront.net/wp-content/uploads/2018/02/fall-apart-roasted-pork-shoulder-319.jpg',
+    //         [
+    //             new Ingredient('Meat', 1),
+    //             new Ingredient('French Fries', 20)
+    //         ]),
+    //     new Recipe('A Test2 Recipe',
+    //         'This is simply a Test2',
+    //         'https://img.taste.com.au/DzZTwETU/taste/2019/06/roast-pork-with-crispy-crackling-and-maple-mustard-pears-p50-150224-3.jpg',
+    //         [
+    //             new Ingredient('Buns', 2),
+    //             new Ingredient('Meat', 1)
+    //         ])
+    //
+    // ];
 
-    ];
+    private recipes: Recipe[] = [];
 
     constructor(private slService: ShoppingListService) {
+    }
 
-
+    setRecipes(recipes: Recipe[]) {
+        this.recipes = recipes;
+        this.recipesChanged.next(this.recipes.slice());
     }
 
 
